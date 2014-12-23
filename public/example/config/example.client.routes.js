6fd85697609e9f433955876dcd61f3ca339fad63
Marco Guzman <mguzman@itexico.net>
2014-12-23 16:51:41 -0600

    Added angular files

---
+++ public/example/config/example.client.routes.js
@@ -0,0 +1,6 @@
+angular.module('example').config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/', { templateURL: 'example/views/example.client.view.html' }).
+      otherwise({ redirectTo: '/' });
+  }
+]);
