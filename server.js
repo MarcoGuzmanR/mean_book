3039aaa8cad4bb14c1d5ba03c06374ed7a1603d0
Marco Guzmán <ganondorf28@gmail.com>
2015-10-26 22:47:47 -0600

    Added behavior for a live chat using Sockets

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var mongoose = require('./config/mongoose'),
     express  = require('./config/express'),
     passport = require('./config/passport');
 
-var db  = mongoose();
-var app = express();
+var db   = mongoose();
+var app  = express(db);
 var passport = passport();
 
 app.listen(3000);
